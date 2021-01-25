a262369a01ac20987b64a09f68f09062b6283b0f
adamazmi2207 <66166999+adamazmi2207@users.noreply.github.com>
2021-01-25 22:33:37 +0900

    Dashboard: Add Top Cards, Sample Charts

--- lib/pages/home_dashboard_view/home_content_desktop.dart
+++ lib/pages/home_dashboard_view/home_content_desktop.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_admin_dashboard_web/theme/color/colors.dart';
+import 'package:flutter_admin_dashboard_web/widgets/charts/bar_chart_widget.dart';
+import 'package:flutter_admin_dashboard_web/widgets/charts/donut_chart.dart';
+import 'package:flutter_admin_dashboard_web/widgets/charts/revenue_chart.dart';
+import 'package:flutter_admin_dashboard_web/widgets/charts/sales_chart.dart';
 
 class HomeContentDesktop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [TopBar(), Body()],
+        children: [
+          TopBar(),
+          Body(),
+        ],
       ),
       backgroundColor: kPrimaryBlack,
     );
@@ -30,48 +37,176 @@ class Body extends StatelessWidget {
             color: kLightBlack,
           ),
           child: Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(25.0),
             child: Column(
               children: [
+                BodyTop(),
+                SizedBox(height: 20),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      'Dashboard',
-                      style: TextStyle(
-                        color: kWhite,
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                      ),
+                    DashboardMiniCard(
+                      icon: Icons.person_outline,
+                      iconColor: Colors.deepOrangeAccent,
+                      title: '2,000',
+                      subTitle: 'Active Users',
+                      accentColor: Colors.deepOrangeAccent.withOpacity(0.1),
                     ),
-                    OutlineButton.icon(
-                      onPressed: () {
-                        print('filters button pressed');
-                      },
-                      borderSide: BorderSide(color: kWhite),
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                      // color: kWhite,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0)),
-                      icon: Icon(
-                        Icons.filter_list_outlined,
-                        color: kWhite,
-                      ),
-                      label: Text(
-                        'Filters',
-                        style: TextStyle(
-                          color: kWhite,
-                        ),
-                      ),
-                    )
+                    DashboardMiniCard(
+                      icon: Icons.attach_money_outlined,
+                      iconColor: kTeal,
+                      title: '\$10,000',
+                      subTitle: 'Total Sales',
+                      accentColor: kTeal.withOpacity(0.1),
+                    ),
+                    DashboardMiniCard(
+                      icon: Icons.report_outlined,
+                      iconColor: Colors.redAccent,
+                      title: '15',
+                      subTitle: 'Unresolved Reports',
+                      accentColor: Colors.redAccent.withOpacity(0.1),
+                    ),
                   ],
                 ),
+                SizedBox(height: 20),
+                Expanded(child: SalesChart()),
+                // BarChartSample1(),
+                // RevenueChart(),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class DashboardMiniCard extends StatelessWidget {
+  final Color accentColor;
+  final IconData icon;
+  final Color iconColor;
+  final String title;
+  final String subTitle;
+  const DashboardMiniCard(
+      {Key key,
+      this.accentColor,
+      this.icon,
+      this.title,
+      this.subTitle,
+      this.iconColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 120,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(18),
+        gradient: LinearGradient(
+          colors: [
+            kPrimaryBlack.withOpacity(0.6),
+            accentColor,
+          ],
+          stops: const [0.70, 4.0],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: kPrimaryBlack.withOpacity(0.8),
+            blurRadius: 6.0,
+            offset: Offset(3, 3),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding:
+            const EdgeInsets.only(left: 20.0, right: 5, bottom: 10, top: 10),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  icon,
+                  size: 40,
+                  color: iconColor,
+                ),
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 22,
+                    letterSpacing: 1.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  subTitle,
+                  style: TextStyle(
+                    color: Colors.white30,
+                  ),
+                ),
+              ],
+            ),
+            IconButton(
+              onPressed: () {
+                print('more Icon Button Clicked');
+              },
+              icon: Icon(Icons.more_vert_rounded),
+              color: Colors.white30,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BodyTop extends StatelessWidget {
+  const BodyTop({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Dashboard',
+          style: TextStyle(
+            color: kWhite,
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.4,
+          ),
+        ),
+        OutlineButton.icon(
+          onPressed: () {
+            print('filters button pressed');
+          },
+          borderSide: BorderSide(color: kWhite),
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+          // color: kWhite,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          icon: Icon(
+            Icons.filter_list_outlined,
+            color: kWhite,
+          ),
+          label: Text(
+            'Filters',
+            style: TextStyle(
+              color: kWhite,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
@@ -84,7 +219,7 @@ class TopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 8),
+      padding: const EdgeInsets.only(top: 20.0, right: 25, bottom: 10),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
